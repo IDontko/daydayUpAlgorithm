4abf9f128802855f8c80bcd5501f1ea1b7e7b5ca
GaoYang <252128492@qq.com>
2021-11-04 11:50:58 +0800

    两数之和，双指针解法

---
+++ src/Array/easy/TwoSumII.java
@@ -0,0 +1,45 @@
+package Array.easy;
+
+/**
+ * @author gaoyang
+ * create on 2021/11/4
+ * 给定一个已按照 非递减顺序排列  的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。
+ * <p>
+ * 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0] < answer[1] <= numbers.length 。
+ * <p>
+ * 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+ * <p>
+ *  
+ * 示例 1：
+ * <p>
+ * 输入：numbers = [2,7,11,15], target = 9
+ * 输出：[1,2]
+ * 解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int low = 0, high = numbers.length - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
+                return new int[]{low + 1, high + 1};
+            } else if (sum < target) {
+                ++low;
+            } else {
+                --high;
+            }
+        }
+        return new int[]{-1, -1};
+
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 25 ,75};
+        TwoSumII twoSumII = new TwoSumII();
+        twoSumII.twoSum(numbers,  100);
+    }
+}
