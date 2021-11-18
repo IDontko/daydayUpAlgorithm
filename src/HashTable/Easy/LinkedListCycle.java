c5313a5a659c6f9f7cc4992c539fd03224f0c4ef
GaoYang <252128492@qq.com>
2021-11-18 15:14:03 +0800

    HasCycle算法。很简单，用快慢指针算法有更好的效果

---
+++ src/HashTable/Easy/LinkedListCycle.java
@@ -0,0 +1,67 @@
+package HashTable.Easy;
+
+import common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author gaoyang
+ * create on 2021/11/18
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ *
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ *
+ * 如果链表中存在环，则返回 true 。 否则，返回 false 。
+
+ *
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        boolean result = false;
+        if (head == null){
+            return result;
+        }
+        ListNode current = head;
+        Set<ListNode> set = new HashSet<>();
+        while (!result &&current != null){
+                if(set.contains(current)){
+                    return true;
+                }
+                set.add(current);
+                current = current.next;
+        }
+        return result;
+    }
+
+    /**
+     * 快慢指针算法
+     * Floyd 判圈算法
+     * @param head
+     * @return
+     */
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+
+
+}
